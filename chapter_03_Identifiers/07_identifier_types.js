6e3e27be89660378054b41c9a1d1392f03ab29f8
shrddha5 <bshrddha94@gmail.com>
2026-05-30 11:14:21 +0530

    Add chapter 2 and chapter 3 folders

---
+++ chapter_03_Identifiers/07_identifier_types.js
@@ -0,0 +1,39 @@
+var name = "Pramod";
+
+var firstName = "Pramod";
+var lastName = "Dutta"; // CamelCase
+
+var first_name = "Amit"; // Snake Case
+
+// Naming Conventions (Cases)
+// ============================================
+// 1. camelCase (standard for JS variables and functions)(//middle variable name start with upper case)
+let userName = "camelCase";
+let totalPrice = 99.99;
+let isLoggedIn = true;
+
+
+
+// 2. PascalCase (standard for JS classes and constructors) (//2 words of variable starts with uppercase  )
+let UserProfile = "PascalCase";
+let ShoppingCart = "class name style";
+
+
+
+// 3. snake_case (underscore separated) (// all laters in variable small case with undersocre(-))
+let user_name = "snake_case";
+let total_price = 49.99;
+let is_logged_in = false;
+
+
+// 4. SCREAMING_SNAKE_CASE (constants)(// all laters in variable capital with undersocre(-))
+const MAX_SIZE = 100;
+const API_KEY = "abc123";
+const DATABASE_URL = "localhost";
+
+
+// 5. Hungarian Notation (prefix with type - older style)(// variable starts with prefix and after prefix word starts with capital laetter)
+let strName = "string prefix";
+let bActive = true;       // boolean
+let nCount = 5;           // number
+let arrItems = [];        // array
