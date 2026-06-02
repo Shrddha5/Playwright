26e724219c01c02509f8492da3bf4456a6996082
Shrddha5 <bshrddha94@gmail.com>
2026-06-02 18:07:33 +0530

    Add chapter 6 operator examples

---
+++ chapter_06_operator/034_Iq.js
@@ -0,0 +1,18 @@
+// Compund Operator
+
+let x = 10;
+x += 10; // x = x +10;
+console.log(x); //20
+
+x -= 3; //  x =x -3
+console.log(x);// 17 // 20-3 = 17
+
+x *= 2; // x = x * 2;
+console.log(x);// 34 // 17*2 = 34
+
+x /= 17;  // x = x/17;
+console.log(x); // 2 // 34/17 = 2
+
+x %= 2; // x = x % 2;
+console.log(x); // 0 // 2 % 2 = 0
+console.log(x);
